ebbbb828f81448c3c0f488b7b5c28232246b5754
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-09-07 12:28:39 +0530

    Feat: Added Cli

---
+++ cli/ayush-cli/README.md
@@ -0,0 +1,16 @@
+## To run This Cli - Developer Mode 
+```bash
+chmod 777 index.js
+./index.js
+```
+
+## How To use Arguments 
+
+```bash
+./index.js <Arguments>
+```
+### Like to Clear the Cli 
+
+```bash
+./index.js -c 
+```
---
+++ cli/ayush-cli/index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+/**
+ * ayush-cli
+ * This Cli uses AyushSync API to connect and App Indian Traditional Medicine and Modern Medicine using FHIR Resources
+ *
+ * @author Mayank-Jha <https://www.linkedin.com/in/mayankkumarjha07/>
+ */
+
+import cli from './utils/cli.js';
+import init from './utils/init.js';
+import log from './utils/log.js';
+
+const { flags, input, showHelp } = cli;
+const { clear, debug } = flags;
+
+(async () => {
+	await init({ clear });
+	debug && log(flags);
+	input.includes(`help`) && showHelp(0);
+})();
