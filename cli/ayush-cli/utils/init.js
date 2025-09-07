a281306d032b4277bb703c080e794fd3e604f4a6
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-09-07 12:50:02 +0530

    Feat: Added Authentication(Frontend)

--- cli/ayush-cli/utils/init.js
+++ cli/ayush-cli/utils/init.js
@@ -1,6 +1,8 @@
 import unhandled from 'cli-handle-unhandled';
 import welcome from 'cli-welcome';
 import { getPackageJson } from 'get-package-json-file';
+import config from './config.js';
+import enquirer from 'enquirer';
 
 export default async ({ clear = true }) => {
 	unhandled();
@@ -16,4 +18,32 @@ export default async ({ clear = true }) => {
 		bold: true,
 		clear
 	});
+
+	let username = config.get('username');
+	let password = config.get('password');
+	let ayushSyncAPICode = config.get('ayushSyncAPICode');
+
+	if (!username || !password || !ayushSyncAPICode) {
+		const questions = [
+			{
+				type: 'input',
+				name: 'username',
+				message: 'Enter your username'
+			},
+			{
+				type: 'password',
+				name: 'password',
+				message: 'Enter your password'
+			},
+			{
+				type: 'input',
+				name: 'ayushSyncAPICode',
+				message: 'Enter your AyushSync API Code'
+			}
+		];
+		const answers = await enquirer.prompt(questions);
+		config.set('username', answers.username);
+		config.set('password', answers.password);
+		config.set('ayushSyncAPICode', answers.ayushSyncAPICode);
+	}
 };
