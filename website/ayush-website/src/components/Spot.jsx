31ab225e4577dd483b077a04901777b5d1c162ee
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-09-20 17:16:18 +0530

    Remove website directory

--- website/ayush-website/src/components/Spot.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Spot = ({ className, style }) => {
-  return <div className={`absolute rounded-full ${className}`} style={style}></div>;
-};
-
-export default Spot;
