65643e8e9414759c71d50edc6a44537641e48659
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-09-20 17:14:12 +0530

    Ignore api, auth, and cli directories

--- cli/ayush-cli/utils/api.js
+++
@@ -1,8 +0,0 @@
-
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://3.26.95.153:8080/api',
-});
-
-export default api;
--- cli/ayush-cli/utils/testModeManager.js
+++
@@ -1,9 +0,0 @@
-let _isTestMode = false;
-
-export const setTestMode = (value) => {
-    _isTestMode = value;
-};
-
-export const getTestMode = () => {
-    return _isTestMode;
-};
